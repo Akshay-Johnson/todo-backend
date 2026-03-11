f0b39b64d41b0806837189011457331700735cc2
Akshay <akshayjohnson117@gmail.com>
2026-03-11 09:41:27 +0530

    todo backend

--- server.js
+++ server.js
@@ -1,26 +1,31 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-
 const taskRoutes = require("./routes/taskRoutes");
-
 const sequelize = require("./config/db");
 require("./models/taskModel");
 
-sequelize.sync().then(() => {
-  console.log("DB synced");
-});
-
 const app = express();
-
-app.use(cors({
-  origin: "http://localhost:5173",
-}));
+app.use(cors);
 app.use(express.json());
 
 app.use("/api/tasks", taskRoutes);
 
 const PORT = process.env.PORT || 5000;
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Database connected...");
+    return sequelize.sync();
+  })
+  .then(() => {
+    console.log("Database synced...");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
