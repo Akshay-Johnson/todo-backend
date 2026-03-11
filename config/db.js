3705a17d158d846bf11a9a43e07e997f37cb236e
Akshay <akshayjohnson117@gmail.com>
2026-03-11 09:32:46 +0530

    connect neon database

--- config/db.js
+++ config/db.js
@@ -1,24 +1,15 @@
 const { Sequelize } = require("sequelize");
-require("dotenv").config();
 
-const sequelize = new Sequelize(
-  process.env_DB_Name,
-  process.env.DB_User,
-  process.env.DB_Password,
-  {
-    host: process.env.DB_Host,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
   },
-);
-
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log("DB connected");
-  })
-  .catch((err) => {
-    console.log("DB error :", err);
-  });
+});
 
 module.exports = sequelize;
